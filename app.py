a2a8720fb5198e3f4b073f1d59f56338e7965ead
MAN5ON <dark34rus@mail.ru>
2020-06-15 19:52:24 +0400

    1.0.3

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
@@ -11,13 +11,25 @@ mongo = PyMongo(app)
 @app.route('/renat', methods=['GET'])
 def index():
     cur = mongo.db.archive.find({}, {'link': 1, 'title': 1, 'text_stat': 1, 'time_date': 1, '_id': 0})
-    return render_template('index.html', cur=list(cur))
+    return render_template('Renat.html', cur=list(cur))
 
 
 @app.route('/ruslan', methods=['GET'])
 def person():
     per = mongo.db.sentence.find({}, {'sentence_person': 1, 'sentence_places': 1, 'news_id': 1, '_id': 0})
-    return render_template('ruslan.html', per=list(per))
+    return render_template('Ruslan.html', per=list(per))
+
+
+@app.route('/ruslanchik', methods=['GET'])
+def synonym():
+    syn = mongo.db.synonym.find({}, {'places': 1, 'person': 1, 'synonyms': 1, '_id': 0})
+    return render_template('Ruslanchik.html', syn=list(syn))
+
+
+@app.route('/vlad', methods=['GET'])
+def classyfic():
+    cly = mongo.db.classify.find({}, {'sentence': 1, 'sentiment': 1, 'link': 1, '_id': 0})
+    return render_template('Vlados.html', cly=list(cly))
 
 
 if __name__ == "__main__":
