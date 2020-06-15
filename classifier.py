366c12f00868be18a5d6cf23051ec1045bb56d3c
FallenHopes <vlad.serebrow@yandex.ru>
2020-06-15 17:20:53 +0400

    Final

--- classifier.py
+++ classifier.py
@@ -2,21 +2,30 @@ from dostoevsky.tokenization import RegexTokenizer
 from dostoevsky.models import FastTextSocialNetworkModel
 from pymongo import MongoClient
 
-def classifier(messages = ['Это анализатор текста из пакета']):
+def classifier(messages = ['ds']):
     tokenizer = RegexTokenizer()
-    tokens = tokenizer.split('всё очень плохо')  # [('всё', None), ('очень', None), ('плохо', None)]
+    tokens = tokenizer.split('всё очень плохо')
     model = FastTextSocialNetworkModel(tokenizer=tokenizer)
     results = model.predict(messages, k=2)
     for message, sentiment in zip(messages, results):
         print(message, '->', sentiment)
+        return {
+            'sentence': message,
+            'sentiment': sentiment
+        }
 
 if __name__ == "__main__":
     client = MongoClient("mongodb+srv://admin:admin@cluster0-y2po7.mongodb.net/vpravda")
     db = client.vpravda
     collection_sentence = db.sentence
+    collection_classify = db.classify
     print("Отношения высказываний к персонам: ")
     for sentence in collection_sentence.find():
-        classifier(sentence['sentence_person'])
+        if (sentence['sentence_person']):
+            persObj  = classifier(sentence['sentence_person'])
+            collection_classify.insert_one(persObj)
     print("Отношения высказываний к местам и достопримечательностям: ")
     for sentence in collection_sentence.find():
-        classifier(sentence['sentence_places'])
+        if (sentence['sentence_places']):
+            placeObj  = classifier(sentence['sentence_places'])
+            collection_classify.insert_one(placeObj)
